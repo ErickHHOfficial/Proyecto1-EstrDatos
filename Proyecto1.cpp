ffbb56f2f1c1692230aaa37ac15ae5b8a49c09b5
ErickHHOfficial <erick.hou@utp.ac.pa>
2024-10-22 22:52:04 -0500

    Update Proyecto1
    cambio al anio
    se agregaron nombres y apellidos nuevos

--- Proyecto1.cpp
+++ Proyecto1.cpp
@@ -15,16 +15,18 @@ struct datosEs{               //Registro con los datos de los Estudiantes
 
 struct datosEs estudiantes[20];          //Arreglo del registro (nos permite guardar 20 registros diferentes)
 
-string nombre[10]{"Colón", "Guillermo", "Carlos", "Omar", "Erick", "Amy", "Estefany", "Hanna", "Caterina", "Luisa"};            //Arreglos con valores string genéricos para la creación de los campos
-string apellido[10]{"Sánchez", "Bárcenas", "Castro", "Leng Leng", "Cedeño", "Franco", "Hou", "Navas", "Chen", "Saenz"};
+string nombre[20]{"Colón", "Guillermo", "Carlos", "Omar", "Erick", "Amy", "Estefany", "Hanna", "Caterina", "Luisa", 
+                    "Winston", "Alex", "Jelena", "Jackeline", "William", "Pablo", "Andrea", "Samuel", "Ana", "Elim"};            //Arreglos con valores string genéricos para la creación de los campos
+string apellido[20]{"Sánchez", "Bárcenas", "Castro", "Leng Leng", "Cedeño", "Franco", "Hou", "Navas", "Chen", "Saenz", 
+                    "Mas", "Zhang", "Yuen", "Barranco", "Torres", "Deng", "Toruño", "Navarro", "Domingo", "Villalobos"};
 string carrera[10]{"Desarrollo & Gestión de Soft.", "Ing. de Soft.", "Ing. Industrial", "Ing. en Alimentos", "Logística & Transporte Multimodal", 
                     "Ing. Civil", "Ing. Electromecánica", "Ciberseguridad", "Refrigeración & AC", "Ing. Marítima Portuaria"};
 
 void datoPE(int i){                        //función que realiza la asignación de valores aleatorios al registro
-    estudiantes[i].nombre = nombre[rand() % 10] + " " + apellido[rand() % 10];
+    estudiantes[i].nombre = nombre[rand() % 20] + " " + apellido[rand() % 20];
     estudiantes[i].cedula = to_string(1 + (rand() % 10)) + "-" + to_string(1 + (rand() % 1999)) + "-" + to_string(1 + (rand() % 999));
     estudiantes[i].mNacimiento = 1 + (rand() % 12);
-    estudiantes[i].anio = 1990 + (rand() % 18);
+    estudiantes[i].anio = 1 + (rand() % 4);
     estudiantes[i].carrera = carrera[rand() % 10];
     estudiantes[i].indice = (float)rand() / RAND_MAX * (3.0 - 1.0) + 1.0;
 };
@@ -35,6 +37,7 @@ void datoPE(int i){                        //función que realiza la asignación
 
     for(int i = 0; i < 20; i++){       //ciclo FOR que llama a la función de asignación
         datoPE(i);
+        cout << estudiantes[i].anio << endl;
     }
 
 return 0;
